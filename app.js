60e190db2e6efca970ac88166dd41f9c704e83e5
ThatphonPH <ppluems2@gmail.com>
2026-04-13 13:55:48 +0700

    added settings for dokku deployment

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const chatRoutes = require('./routes/chatRoutes');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json()); // Middleware for parsing JSON requests
